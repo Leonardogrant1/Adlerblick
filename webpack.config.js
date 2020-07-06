f4afc6bfc463c455162ec62ea531b8970a2ae11f
yorrd <kai@adornis.de>
2020-07-06 10:23:20 +0200

    fix: clean up some config

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,69 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
- 
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
-    entry: {
-        app: [
-        './src/index.js']
-    },
-    output: {
-        path: __dirname + "dist",
-        filename: 'bundle.js'
-    },
+  entry: {
+    app: ["./src/index.js"],
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
 
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        historyApiFallback: true
-    },
-    // module: {
-    //     rules: [{
-    //         test: /\.html$/,
-    //         use: [
-    //             {
-    //                 loader: "html-loader",
-    //                 options: { minimize: true }
-    //             }
-    //         ]
-    //     }]
-    // },
-module: {
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    historyApiFallback: true,
+  },
+  // module: {
+  //     rules: [{
+  //         test: /\.html$/,
+  //         use: [
+  //             {
+  //                 loader: "html-loader",
+  //                 options: { minimize: true }
+  //             }
+  //         ]
+  //     }]
+  // },
+  module: {
     rules: [
+      // {
+      //   test: /\.html$/, // tells webpack to use this loader for all ".html" files
+      //   use: [{ loader: "html-loader" }],
+      // },
       {
-        test: /\.html$/, // tells webpack to use this loader for all ".html" files
-        loader: 'html-loader'
-    },
-        {
-            test: /\.(png|jpg|gif)$/i,
-            loader: 'file-loader',
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
             options: {
-              name: '[path][name].[ext]',
+              name: "[path][name].[ext]",
             },
           },
-          {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env'],
-                plugins: ['@babel/plugin-transform-runtime', '@babel/plugin-proposal-object-rest-spread'],
-                
-              },
-            
-            }
-          }
-      ]}
-      ,
-
-    plugins: [
-        new HtmlWebpackPlugin(), 
-        new FaviconsWebpackPlugin('./public/images/favicon.png')
-
-
-    ]
-}
-
-
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-transform-runtime",
+              "@babel/plugin-proposal-object-rest-spread",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new FaviconsWebpackPlugin("./public/images/favicon.png"),
+  ],
+};
